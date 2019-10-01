876429c315d84fc76db22acfa9d86c30d4691b34
root <root@L1C13X.lab.cct.uenp.edu.br>
2019-10-01 20:21:42 -0300

    Holy Fix

--- doshit.sh
+++ doshit.sh
@@ -3,6 +3,7 @@ CLASSPATH="deps/antlr-4.7.2-complete.jar"
 alias antlr4='java -Xmx500M -cp "$CLASSPATH" org.antlr.v4.Tool'
 alias grun='java -Xmx500M -cp "$CLASSPATH" org.antlr.v4.gui.TestRig'
 
+mkdir -p ./build
 antlr4 ./src/parser/grammar/Calc.g4 -Xexact-output-dir -o ./src/parser/generated_code/
 javac -cp "$CLASSPATH" ./src/compiler/Utils.java -d ./build
 javac -cp "./build:$CLASSPATH" ./src/parser/generated_code/*.java -d ./build
